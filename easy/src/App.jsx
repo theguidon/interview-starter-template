6a26695deef41b9205ef2459898a8db2195fcc35
Cheska Huang <julyannahuang@gmail.com>
2026-06-29 13:22:26 +0800

    fixed fonts for easy

--- easy/src/App.jsx
+++ easy/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1 className="font-sans">Get started</h1>
+          <h1 className="font-display">Get started</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
